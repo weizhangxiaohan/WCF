f884c4d420088c86b677c3ce06857e66d8424ee0
WeiZhangXiaoHan_Home <191689072@qq.com>
2016-04-27 23:00:49 +0800

    home

--- CustomerInterface/ICustomer.cs
+++ CustomerInterface/ICustomer.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomerInterface
 {
+    [ServiceContract]
     public interface ICustomer
     {
-        string GetQ();
+        [OperationContract]
+        int RegisterCustomer(Customer customer);
+    }
+
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public string CustomerName { get; set; }
+        [DataMember]
+        public string CustomerFirstName { get; set; }
+        [DataMember]
+        public string CustomerMiddleLetter { get; set; }
+        [DataMember]
+        public DateTime CustomerBirthDate { get; set; }
     }
 }
